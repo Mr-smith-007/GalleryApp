0ac2216441af0962b3676f41fc02f99f21db3729
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-05-15 12:37:47 +0300

    на проверку

--- GalleryApp/GalleryApp/App.xaml.cs
+++ GalleryApp/GalleryApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GalleryApp.Page;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace GalleryApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new LoginPage());
         }
 
         protected override void OnStart()
